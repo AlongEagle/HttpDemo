bd629b6284fdfc16dd7c4ceb6771fbd2a61fa120
AlongEagle <1107635271@qq.com>
2015-12-02 21:46:32 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/httpdemo/HttpThread.java
@@ -0,0 +1,42 @@
+package com.example.administrator.httpdemo;
+
+import android.webkit.WebView;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Handler;
+
+/**
+ * Created by Administrator on 2015-11-29.
+ */
+public class HttpThread extends Thread {
+
+    private String url;
+    private WebView webView;
+    private Handler handler;
+
+
+    public HttpThread(String url,WebView webView,Handler handler){
+        this.url=url;
+        this.webView=webView;
+        this.handler=handler;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            URL httpURL=new URL(url);
+            HttpURLConnection conn= (HttpURLConnection) httpURL.openConnection();
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
