edbb92e897ac7181afe3f48b44518cf79d171ac2
AlongEagle <1107635271@qq.com>
2015-12-03 20:42:19 +0800

    修改了kiiu

--- app/src/main/java/com/example/administrator/httpdemo/kiiu.java
+++ app/src/main/java/com/example/administrator/httpdemo/kiiu.java
@@ -4,4 +4,23 @@ package com.example.administrator.httpdemo;
  * Created by Administrator on 2015-12-03.
  */
 public class kiiu {
+    public void funm(){
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+        System.out.println("this is a demo to find some to  login github!!");
+    }
 }
